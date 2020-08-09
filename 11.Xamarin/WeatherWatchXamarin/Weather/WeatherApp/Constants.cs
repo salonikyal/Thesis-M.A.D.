39dc1dcdf7073683b1bf9ec100c9d254b4db6f1e
salonikyal <salonikyal1996@gmail.com>
2020-08-09 22:50:22 +0530

    Xamarin added

---
+++ 11.Xamarin/WeatherWatchXamarin/Weather/WeatherApp/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "e25a68c009aa630161457691d6f0a5a6";
+    }
+}
---
+++ 11.Xamarin/WeatherWatchXamarin/Weather/WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        RestService _restService;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _restService = new RestService();
+        }
+
+
+        async void OnGetWeatherButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
+            {
+                WeatherData weatherData = await _restService.GetWeatherData(GenerateRequestUri(Constants.OpenWeatherMapEndpoint));
+                BindingContext = weatherData;
+            }
+        }
+
+        string GenerateRequestUri(string endpoint)
+        {
+            string requestUri = endpoint;
+            requestUri += $"?q={_cityEntry.Text}";
+            requestUri += "&units=metric"; // or units=metric
+            requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+            return requestUri;
+        }
+		
+		
+    }
+	
+	 public class WeatherList
+    {
+        public string Temp { get; set; }
+        public string Date { get; set; }
+        
+    }
+}
