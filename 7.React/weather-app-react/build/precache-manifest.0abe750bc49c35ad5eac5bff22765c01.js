f9bebdcc8fe73f16e21fe46e7118654a3b0ca4a8
salonikyal <salonikyal1996@gmail.com>
2020-09-11 03:42:00 +0530

    pwa-react added

---
+++ 7.React/weather-app-react/build/precache-manifest.0abe750bc49c35ad5eac5bff22765c01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc70225df89c1bf00c22425791ba4806",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "8441114019b497e84137",
+    "url": "/weather-app-react/static/css/main.ae57d594.chunk.css"
+  },
+  {
+    "revision": "df64f1c5b10c377ce881",
+    "url": "/weather-app-react/static/js/2.5da6d82c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/weather-app-react/static/js/2.5da6d82c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8441114019b497e84137",
+    "url": "/weather-app-react/static/js/main.15d0eaaa.chunk.js"
+  },
+  {
+    "revision": "d9885b805ae5faf1bd40",
+    "url": "/weather-app-react/static/js/runtime-main.cdab807e.js"
+  },
+  {
+    "revision": "f1ddd84e18c0bb2e087ad838d1791c99",
+    "url": "/weather-app-react/static/media/2850815.f1ddd84e.jpg"
+  }
+]);
